4dfd4ea08981a774a717b01e4a388c4840b31ea3
Guillaume <guillaume.guerdoux@student.ecp.fr>
2017-01-15 20:03:54 +0100

    change rules for tic tac toe 4x4 : line of 3 to win

--- tic_tac_toe_4x4.py
+++ tic_tac_toe_4x4.py
@@ -4,11 +4,23 @@ from artificial_intelligence import ArtificialIntelligence
 
 class TicTacToe4x4:
     winning_combos = (
-        {0, 1, 2, 3}, {4, 5, 6, 7}, {8, 9, 10, 11},
-        {12, 13, 14, 15},
-        {0, 4, 8, 12}, {1, 5, 9, 13}, {2, 6, 10, 14},
-        {3, 7, 11, 15},
-        {0, 5, 10, 15}, {3, 6, 9, 12})
+        # Lines
+        {0, 1, 2}, {1, 2, 3},
+        {4, 5, 6}, {5, 6, 7},
+        {8, 9, 10}, {9, 10, 11},
+        {12, 13, 14}, {13, 14, 15},
+
+        # Columns
+        {0, 4, 8}, {4, 8, 12},
+        {1, 5, 9}, {5, 9, 13},
+        {2, 6, 10}, {6, 10, 14},
+        {3, 7, 11}, {7, 11, 15},
+
+        # Diago
+        {0, 5, 10}, {5, 10, 15},
+        {3, 6, 9}, {6, 9, 12},
+        {1, 6, 11}, {4, 9, 14},
+        {2, 5, 8}, {7, 10, 13})
 
     winners = ('X win', 'Draw', 'O win')
 
@@ -71,8 +83,8 @@ class TicTacToe4x4:
         return self.winners[1]
 
     def heuristic(self, player):
-        X_score = 10
-        O_score = 10
+        X_score = 24
+        O_score = 24
         for winning_combo in self.winning_combos:
             if self.X_set.intersection(winning_combo):
                 O_score -= 1
